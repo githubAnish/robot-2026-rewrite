1cd123cd6ac0f45363363f52baf2962d04e7def0
theGitKrakenAnish <malrajanish@gmail.com>
2026-03-03 17:12:13 -0500

    Remove faulty intake assist logic, need to figure problems out

--- src/main/java/org/frogforce503/robot/commands/IntakeFuelFromGround.java
+++ src/main/java/org/frogforce503/robot/commands/IntakeFuelFromGround.java
@@ -11,14 +11,10 @@ import org.frogforce503.robot.subsystems.superstructure.intakeroller.IntakeRolle
 import org.frogforce503.robot.subsystems.superstructure.intakeroller.IntakeRollerConstants;
 import org.frogforce503.robot.subsystems.vision.Vision;
 import org.ironmaple.simulation.IntakeSimulation;
-import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -37,15 +33,6 @@ public class IntakeFuelFromGround extends Command {
 
     // Constants
     private final double kLookaheadTimeSec = 0.15;
-
-    private final double kAssistMaxDistance = Units.inchesToMeters(90);
-    private final double kMaxAssistStrafeSpeed = 0.7;
-    private final double kMaxAssistOmega = 2.0;
-
-    private final double kThetaAssistGain = 4.0;
-
-    // Controllers
-    private final PIDController lateralAssistController = new PIDController(1.2, 0.0, 0.0);
 
     // Sim
     private final IntakeSimulation intakeSimulation;
@@ -105,40 +92,12 @@ public class IntakeFuelFromGround extends Command {
                 drive.getAngle());
 
         // Assist logic (normal teleop drive if auto assist not wanted)
-        double yError = 0.0;
-        double yOutput = 0.0;
-        double distance = 0.0;
-
         if (autoAssistEnabled.getAsBoolean()) {
-            Translation2d toTargetField = target.minus(robotPose.getTranslation());
-            Translation2d toTargetRobot = toTargetField.rotateBy(robotPose.getRotation().unaryMinus());
 
-            distance = toTargetField.getNorm();
-            double distanceScale = MathUtil.clamp((kAssistMaxDistance - distance) / kAssistMaxDistance, 0.0, 1.0);
-
-            // Lateral assist
-            yError = toTargetRobot.getY();
-            double strafeOverride = MathUtil.clamp(1.0 - Math.abs(yVelocity), 0.0, 1.0);
-            yOutput = lateralAssistController.calculate(yError, 0.0) * distanceScale * strafeOverride;
-            yOutput = MathUtil.clamp(yOutput, -kMaxAssistStrafeSpeed, kMaxAssistStrafeSpeed);
-
-            speeds.vyMetersPerSecond += yOutput;
-
-            // Rotation assist
-            double headingError = toTargetField.getAngle().minus(robotPose.getRotation()).getRadians();
-            double omegaOverride = MathUtil.clamp(1.0 - Math.abs(omega), 0.0, 1.0);
-            double omegaAssist = MathUtil.clamp(headingError * kThetaAssistGain, -kMaxAssistOmega, kMaxAssistOmega);
-
-            speeds.omegaRadiansPerSecond += omegaAssist * distanceScale * omegaOverride;
         }
 
         // Apply speeds
         drive.runVelocity(speeds);
-
-        // Log data
-        Logger.recordOutput("IntakeFuelFromGround/YError", autoAssistEnabled.getAsBoolean() ? new double[] { yError } : new double[] {});
-        Logger.recordOutput("IntakeFuelFromGround/YOutput", autoAssistEnabled.getAsBoolean() ? new double[] { yOutput } : new double[] {});
-        Logger.recordOutput("IntakeFuelFromGround/Distance", autoAssistEnabled.getAsBoolean() ? new double[] { distance } : new double[] {});
     }
 
     @Override
